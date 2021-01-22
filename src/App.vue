0b685a1cbe280e65e4d20a63ab1e4d5966af5508
r-z-x <844818249@qq.com>
2021-01-22 18:48:34 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+
+      <div class="p-3 bg-green-500 text-white a">
+        Hello Tailwind CSS
+        <div class="p-3 bg-red-500 text-white b">Hello Tailwind CSS
+          <div class="p-3 bg-blue-500 text-white c">Hello Tailwind CSS</div>
+        </div>
+      </div>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="less" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  .a{
+    font-size: 30px;
+    .b{
+      font-size: 16px;
+      .c{
+        font-size: 20px;
+      }
+    }
+  }
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
